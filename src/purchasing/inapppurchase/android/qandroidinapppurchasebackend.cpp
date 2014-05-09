080d4b8a1a1981de6f80fcc1e3c72a516feb41f1
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@digia.com>
2014-05-09 08:10:23 +0200

    Remove debug output
    Change-Id: Id3529470ba19e834a389445bdbb6918f4d9875cd
    Reviewed-by: Andy Nichols <andy.nichols@digia.com>

--- src/purchasing/inapppurchase/android/qandroidinapppurchasebackend.cpp
+++ src/purchasing/inapppurchase/android/qandroidinapppurchasebackend.cpp
@@ -32,7 +32,7 @@
 
 QT_BEGIN_NAMESPACE
 
-#define QANDROIDINAPPPURCHASEBACKEND_DEBUG
+// #define QANDROIDINAPPPURCHASEBACKEND_DEBUG
 
 QAndroidInAppPurchaseBackend::QAndroidInAppPurchaseBackend(QObject *parent)
     : QInAppPurchaseBackend(parent)
