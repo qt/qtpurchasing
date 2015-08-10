e30df2ce13e7d1ec6add8bb0ea4f96217246ec3e
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@theqtcompany.com>
2015-08-10 13:22:24 +0200

    Remove GPLv2 from license options
    The GPLv2 license is not compatible with the Apache license,
    which is the license used for some of the Android code, and
    having different licenses on different target platforms is not
    a good solution for the end-users, so instead we remove GPLv2
    from the license options and go with the standard LGPLv3+Commercial
    dual license.
    
    Change-Id: I1112fab94c1bb4d1b3a3edaae0fbf58bba38cce8
    Reviewed-by: Andy Nichols <andy.nichols@theqtcompany.com>

--- src/purchasing/inapppurchase/qinappstore_p.h
+++ src/purchasing/inapppurchase/qinappstore_p.h
@@ -5,7 +5,7 @@
 **
 ** This file is part of the Purchasing module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL3$
+** $QT_BEGIN_LICENSE:LGPL3-COMM$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
@@ -21,14 +21,6 @@
 ** packaging of this file. Please review the following information to
 ** ensure the GNU Lesser General Public License version 3 requirements
 ** will be met: https://www.gnu.org/licenses/lgpl.html.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 2.0 or later as published by the Free
-** Software Foundation and appearing in the file LICENSE.GPL included in
-** the packaging of this file. Please review the following information to
-** ensure the GNU General Public License version 2.0 requirements will be
-** met: http://www.gnu.org/licenses/gpl-2.0.html.
 **
 ** $QT_END_LICENSE$
 **
